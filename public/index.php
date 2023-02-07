07235e11bc95954cc04ff0502a8f3aaf421bcef9
Allbertss <albertasgrigoryan@gmail.com>
2023-02-07 21:44:36 +0000

    request static class createFromGlobals

--- public/index.php
+++ public/index.php
@@ -2,4 +2,14 @@
 
 declare(strict_types=1);
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+$request = \allberts\core\Http\Request::createFromGlobals();
+
+dd($request);
+
 echo "hi";
